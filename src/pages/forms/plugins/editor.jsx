34b322586caabfa61bb3739c7f06613737bcd52a
Saka Sheriff Alade <sheriffdeenalade@gmail.com>
2025-06-20 21:00:10 +0100

    edited the editor.js file

--- src/pages/forms/plugins/editor.jsx
+++ src/pages/forms/plugins/editor.jsx
@@ -6,7 +6,7 @@ import Grid from '@mui/material/Grid2';
 // project imports
 import ReactQuill from 'components/third-party/ReactQuill';
 import MainCard from 'components/MainCard';
-import ReactDraft from 'sections/forms/plugins/ReactDraft';
+// import ReactDraft from 'sections/forms/plugins/ReactDraft';
 import { ThemeDirection } from 'config';
 
 // ==============================|| PLUGINS - EDITOR ||============================== //
@@ -15,19 +15,19 @@ export default function Editor() {
   const [text, setText] = useState(
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   );
-  const [ReactDraft, setReactDraft] = useState(null);
+  // const [ReactDraft, setReactDraft] = useState(null);
 
   const handleChange = (value) => {
     setText(value);
   };
 
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      import('./plugins/ReactDraft').then((mod) => {
-        setReactDraft(() => mod.default);
-      });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (process.env.NODE_ENV !== 'production') {
+  //     import('./plugins/ReactDraft').then((mod) => {
+  //       setReactDraft(() => mod.default);
+  //     });
+  //   }
+  // }, []);
 
   return (
     <Grid container spacing={3}>
@@ -83,7 +83,7 @@ export default function Editor() {
       >
         <MainCard title="React Draft" sx={{ overflow: 'visible', '& .rdw-editor-wrapper': { overflow: 'visible' } }}>
           {/* <ReactDraft /> */}
-          {ReactDraft ? <ReactDraft /> : null}
+          {/* {ReactDraft ? <ReactDraft /> : null} */}
         </MainCard>
       </Grid>
       <Grid size={12}>
