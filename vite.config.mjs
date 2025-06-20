c5faf4b470411ad2756494e03c10248529943b3a
Saka Sheriff Alade <sheriffdeenalade@gmail.com>
2025-06-20 19:07:52 +0100

    after editing vite config mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
-import csslogger from 'postcss-preloader';
+// import csslogger from 'postcss-preloader';
 import { VitePWA } from 'vite-plugin-pwa';
 import manifest from './manifest.json';
 
